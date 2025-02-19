4d6572c081e289e4fc6500aa7cabd47eb2064eb9
Miles Georgi <azimux@gmail.com>
2025-02-19 09:13:51 -0800

    Create ruby project files

---
+++ spec/foobara/cached_command_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::CachedCommand do
+  it "has a version number" do
+    expect(Foobara::CachedCommand::VERSION).to_not be_nil
+  end
+end
---
+++ src/foobara/cached_command.rb
@@ -0,0 +1,4 @@
+module Foobara
+  module CachedCommand
+  end
+end
